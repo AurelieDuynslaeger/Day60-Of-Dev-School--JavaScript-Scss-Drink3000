54e9efe1df1fd415df062de758e43def27ab9ed1
AureLeeli <aduynslaeger@gmail.com>
2023-12-15 15:45:59 +0100

    fix avant merge vers main

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//header qui apparait au scroll
+//récup de la nav par l'id
+const navbar = document.getElementById('navbar');
+//evenement au scroll >100px et action de la navbar
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+        navbar.style.display = 'block'; 
+        navbar.style.position = 'fixed'; 
+        navbar.style.top = '0'; 
+    } else {
+        navbar.style.display = 'none'; 
+    }
+});
+
+//filtrage boissons
+// récup des btn qui ont la class "energisante" et "désaltérante"
+let energizFilter = document.querySelector('.energisantes');
+let coolFilter = document.querySelector('.desalterantes');
+
+energizFilter.addEventListener('click', ()=>{
+    //les div ayant la class cooling/desalterante deviennent grisées et leur contenu change non disponible
+});
+
+coolFilter.addEventListener('click', ()=>{
+    //les div ayant la class energize/energisante deviennent grisées et leur contenu change non disponible
+});
+
+//selection boissons
+//récup de tous les boutons ".selectionner"
+let selectBtn = document.querySelectorAll('.selectionner');
+
+//récup de l'id de la div dont le contenu changera pour afficher le nom de la boisson selectionnée
+let selectedDrink = document.querySelector('#selectedDrink');
+
+//selectBtn = [] tableau de tous les boutons ayant la class ".selectionner"
+//écouteur sur chaque btn
+selectBtn.forEach(bouton => {
+    bouton.addEventListener('click', function() {
+        let nomBoisson = this.parentElement.parentElement.querySelector('h3').innerText;
+        //ici ca récup notre h3 de la carte boisson 'original', 'mojito', 'tropical'...pour le mettre dans la div affichage
+        selectedDrink.innerText = `Boisson sélectionnée : ${nomBoisson}`;
+    });
+});
+
+
+
+
